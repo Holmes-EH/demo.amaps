2387465d04b34915877ec5d2fb94cc59aeac1a8c
Holmes-EH <holmes.samuel@yahoo.fr>
2021-11-04 21:54:12 +0100

    move get session to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Login from './components/Login/Login'
 import Landing from './components/Landing/Landing'
 import { store } from './store'
-import { useContext } from 'react'
+import axios from 'axios'
+import { useContext, useEffect } from 'react'
 
 import { Switch, Route } from 'react-router-dom'
 
@@ -12,7 +13,40 @@ import Layout from './components/Layout/Layout'
 
 function App() {
 	const globalState = useContext(store)
+	const { dispatch } = globalState
 	const { user, amap } = globalState.state
+
+	useEffect(() => {
+		const getSession = async () => {
+			dispatch({ type: 'LOADING' })
+			try {
+				const config = {
+					headers: {
+						Authorization: `Bearer ${user.token}`,
+					},
+				}
+				const { data } = await axios.get(
+					`${process.env.REACT_APP_API_URL}/api/sessions?current=true`,
+					config
+				)
+				dispatch({ type: 'SET_SESSION', payload: data })
+				dispatch({ type: 'FINISHED_LOADING' })
+			} catch (error) {
+				dispatch({
+					type: 'MESSAGE',
+					payload:
+						error.response && error.response.data.message
+							? error.response.data.message
+							: error.message,
+					messageType: 'error',
+				})
+				dispatch({ type: 'FINISHED_LOADING' })
+			}
+		}
+		if (user.token) {
+			getSession()
+		}
+	}, [dispatch, user.token])
 
 	const getRandomInt = (max) => {
 		return Math.floor(Math.random() * max)
