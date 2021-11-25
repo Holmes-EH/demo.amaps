b8aefea8ccf84dc76fb4c7a386600cd982b7494d
Holmes-EH <holmes.samuel@yahoo.fr>
2021-11-25 14:43:05 +0100

    move background image to indexjs

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import App from './App'
 import { StateProvider } from './store'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+const getRandomInt = (max) => {
+	return Math.floor(Math.random() * max)
+}
+
+const backgroundImageUrl = () => {
+	return `/images/${getRandomInt(6)}.jpg`
+}
+
 ReactDOM.render(
 	<StateProvider>
 		<Router>
-			<App />
+			<div
+				className='main flex'
+				style={{ backgroundImage: `url(${backgroundImageUrl()})` }}
+			>
+				<App />
+			</div>
 		</Router>
 	</StateProvider>,
 	document.getElementById('root')
